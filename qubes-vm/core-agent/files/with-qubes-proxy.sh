3d3aaba61eacc0864c83dd6ca1df2a9e7e6d1f6e
Alexander D <maniac@pzskc383.dp.ua>
2019-09-02 07:25:11 +0300

    add manpages, other requested fixes

--- qubes-vm/core-agent/files/with-qubes-proxy.sh
+++ qubes-vm/core-agent/files/with-qubes-proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f /var/run/qubes/this-is-templatevm ]; then
+if [ -f /var/run/qubes-service/updates-proxy-setup ]; then
     export http_proxy=127.0.0.1:8082
     export https_proxy=127.0.0.1:8082
 fi
